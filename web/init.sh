1bc341073e60ac33cf14b27e20456d8a676922fb
Aleksey Lavrenov <aliadm.net@gmail.com>
2020-07-05 13:12:45 +0000

    second lesson

--- web/init.sh
+++ web/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/h.py /etc/gunicorn.d/test.py
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
