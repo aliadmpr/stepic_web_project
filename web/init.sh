abf7f9d5df209cb928bc9c4619d8d4a77f8eae57
aliadm <aliadm.net@gmail.com>
2020-07-05 23:56:07 +0000

    допилил init.sh

--- web/init.sh
+++ web/init.sh
@@ -4,4 +4,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/h.py /etc/gunicorn.d/test.py
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
+sudo gunicorn -b 0.0.0.0:8080 hello:app
