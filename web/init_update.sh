f094712099c95d6db0440ab1512644518bae100d
aliadm <aliadm.net@gmail.com>
2020-07-16 02:53:33 -0700

    add init_update.sh

---
+++ web/init_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+sudo apt update
+sudo apt install python3.5
+sudo apt-get install -y python3.5-dev
+sudo rm /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo -H pip3 install --upgrade django==2.1
+sudo -H pip3 install --upgrade gunicorn
